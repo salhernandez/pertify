3c53e7b1c82697ec36d300162b71dfa9238e791a
Salvador Hernandez <hernandezg.sal@gmail.com>
2021-01-10 22:00:50 -0800

    Add material ui containers

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import Paper from "@material-ui/core/Paper";
+import Box from "@material-ui/core/Box";
+import Container from "@material-ui/core/Container";
 import React, { Component } from "react";
 
 class App extends Component {
     render(){
 
         return (
-            <Paper>
-                stuff
-            </Paper>
+            <Container>
+                <Box>
+                    <Paper>
+                        <div>stuff</div>
+                    </Paper>
+                </Box>
+            </Container>
         );
-}
+    }
 }
 
 export default App;
