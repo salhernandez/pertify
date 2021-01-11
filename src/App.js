d594529bcb2e540e0fcaa724d0e86af01e225e00
Salvador Hernandez <hernandezg.sal@gmail.com>
2021-01-11 00:36:04 -0800

    Add properly spaced cards + paper

--- src/App.js
+++ src/App.js
@@ -3,33 +3,39 @@ import Box from "@material-ui/core/Box";
 import Container from "@material-ui/core/Container";
 import React, { Component, Fragment } from "react";
 import SubTasks from "./SubTasks";
-import { Card, CardHeader  } from '@material-ui/core';
+import { Card, CardHeader, Grid } from '@material-ui/core';
 
 class App extends Component {
     render(){
 
         return (
             <Container>
-                <Box>
-                    <Paper>
-                        <Card>
-                            <CardHeader 
-                                title="Task 1"
-                                subheader={
-                                    <Fragment>
-                                        <div>
-                                            μ: stuff
-                                        </div>
-                                        <div>
-                                            σ: stuff
-                                        </div>
-                                    </Fragment>
-                                }
-                            />
-                            <SubTasks/>
-                        </Card>
-                    </Paper>
-                </Box>
+                <Grid item xs={12}>
+                    <Grid container justify="center" spacing={10}>
+                        {[0, 1, 2].map((value) => (
+                            <Grid key={value} item>
+                                <Paper elevation={3}>
+                                    <Card>
+                                        <CardHeader 
+                                            title="Task 1"
+                                            subheader={
+                                                <Fragment>
+                                                    <div>
+                                                        μ: stuff
+                                                    </div>
+                                                    <div>
+                                                        σ: stuff
+                                                    </div>
+                                                </Fragment>
+                                            }
+                                        />
+                                        <SubTasks/>
+                                    </Card>
+                                </Paper>
+                            </Grid>
+                        ))}
+                    </Grid>
+                </Grid>
             </Container>
         );
     }
