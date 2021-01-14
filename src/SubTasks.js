236e618b8487d57401f2168389e364bbb71a2b3c
Salvador Hernandez <hernandezg.sal@gmail.com>
2021-01-13 22:59:50 -0800

    Remove unused code

--- src/SubTasks.js
+++ src/SubTasks.js
@@ -1,113 +1,20 @@
 import React, { useState } from "react";
 import Paper from "@material-ui/core/Paper";
-import OutlinedInput from "@material-ui/core/OutlinedInput";
-import Select from "@material-ui/core/Select";
-import MenuItem from "@material-ui/core/MenuItem";
-import Typography from "@material-ui/core/Typography";
-import GridMUI from "@material-ui/core/Grid";
-import Checkbox from "@material-ui/core/Checkbox";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import { withStyles } from "@material-ui/core/styles";
 import { EditingState } from "@devexpress/dx-react-grid";
 import {
     Grid,
     Table,
     TableHeaderRow,
     TableInlineCellEditing,
-    Toolbar,
     TableEditColumn,
     TableEditRow
 } from "@devexpress/dx-react-grid-material-ui";
-
-import {
-    Plugin,
-    Template,
-    TemplatePlaceholder,
-} from "@devexpress/dx-react-core";
 
 import {
     generateRows,
 } from "./demo-data/generator";
 
 const getRowId = row => row.id;
-
-const styles = () => ({
-    input: {
-        fontSize: "14px",
-        width: "90px",
-    },
-    label: {
-        fontSize: "14px",
-    },
-    container: {
-        maxWidth: "18em",
-    },
-    selector: {
-        height: "32px",
-    },
-});
-
-const StartEditActionSelectorBase = (props) => {
-    const { defaultAction, changeAction, classes } = props;
-    return (
-        <GridMUI
-            container
-            alignItems="center"
-            className={classes.container}
-        >
-            <Typography
-                className={classes.label}
-            >
-        Start Edit Action:
-        &nbsp;
-            </Typography>
-            <Select
-                onChange={e => changeAction(e.target.value)}
-                value={defaultAction}
-                className={classes.selector}
-                input={(
-                    <OutlinedInput
-                        classes={{ input: classes.input }}
-                        labelWidth={0}
-                        margin="dense"
-                    />
-                )}
-            >
-                <MenuItem value="click">Click</MenuItem>
-                <MenuItem value="doubleClick">Double Click</MenuItem>
-            </Select>
-        </GridMUI>
-    );
-};
-const StartEditActionSelector = withStyles(styles, { name: "StartEditActionSelector" })(StartEditActionSelectorBase);
-
-const SelectTextCheckerBase = (props) => {
-    const { isSelectText, changeSelectText, classes } = props;
-    return (
-        <FormControlLabel
-            control={(
-                <Checkbox
-                    checked={isSelectText}
-                    onChange={e => changeSelectText(e.target.checked)}
-                    color="primary"
-                />
-            )}
-            classes={{ label: classes.label }}
-            label="Select Text On Focus"
-        />
-    );
-};
-const SelectTextChecker = withStyles(styles, { name: "SelectTextChecker" })(SelectTextCheckerBase);
-
-const EditPropsPanel = props => (
-    <Plugin name="EditPropsPanel">
-        <Template name="toolbarContent">
-            <SelectTextChecker {...props} />
-            <TemplatePlaceholder />
-            <StartEditActionSelector {...props} />
-        </Template>
-    </Plugin>
-);
 
 const FocusableCell = ({ onClick, ...restProps }) => (
     <Table.Cell {...restProps} tabIndex={0} onFocus={onClick} />
@@ -130,7 +37,6 @@ const computeExpectedDuration = ({
 
 const computeStandardDeviation = ({
     optimistic,
-    nominal,
     pessimistic
 }) => {
 
@@ -152,8 +58,8 @@ export default (props) => {
         { name: "standardDeviation", title: "σ" },
     ]);
     const [rows, setRows] = useState(generateRows());
-    const [startEditAction, setStartEditAction] = useState("click");
-    const [selectTextOnEditStart, setSelectTextOnEditStart] = useState(true);
+    const [startEditAction] = useState("click");
+    const [selectTextOnEditStart] = useState(true);
     const [editingStateColumnExtensions] = useState([
         { columnName: "expectedDuration", editingEnabled: false },
         { columnName: "standardDeviation", editingEnabled: false },
@@ -223,19 +129,12 @@ export default (props) => {
                 />
                 <Table cellComponent={FocusableCell} />
                 <TableHeaderRow />
-                <Toolbar />
-                <EditPropsPanel
-                    defaultAction={startEditAction}
-                    changeAction={setStartEditAction}
-                    isSelectText={selectTextOnEditStart}
-                    changeSelectText={setSelectTextOnEditStart}
-                />
                 <TableInlineCellEditing
                     startEditAction={startEditAction}
                     selectTextOnEditStart={selectTextOnEditStart}
                 />
                 <TableEditRow />
-                <TableEditColumn showAddCommand showDeleteCommand />
+                <TableEditColumn showAddCommand showDeleteCommand/>
             </Grid>
         </Paper>
     );
