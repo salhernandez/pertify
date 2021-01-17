da56351442b5aa964ede7a8b14402f4b005e40a6
Salvador Hernandez <hernandezg.sal@gmail.com>
2021-01-16 16:30:28 -0800

    Add Header, subtile and placeholder body

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import Paper from "@material-ui/core/Paper";
 import Container from "@material-ui/core/Container";
 import React, { Component, Fragment } from "react";
 import SubTasks from "./SubTasks";
-import { Card, CardHeader, Grid, Fab, TextField, CardActions } from "@material-ui/core";
+import { Card, CardHeader, Grid, Fab, TextField, CardActions, Typography, Box } from "@material-ui/core";
 import AddIcon from "@material-ui/icons/Add";
 import Button from "@material-ui/core/Button";
 import { v4 as uuidv4 } from "uuid";
+const gridSpacing = 10;
 
 
 class App extends Component {
@@ -117,61 +118,91 @@ class App extends Component {
     render(){
         const {tasks} = this.state;
         return (
-            <Container>
-                <Grid item xs={12}>
-                    <Grid container justify="center" spacing={10}>
+            <Box> 
+                <Container
+                    disableGutters={true}
+                    style={{
+                        // grid spacing is base don spacing * 8px
+                        marginBottom: `${(gridSpacing / 2) * 8}px`
+                    }}
+                >
+                    <Typography 
+                        variant="h1" 
+                        align="center"
+                    >
+                        PERTify
+                    </Typography>
 
-                        {tasks.map((task) => (
-                            <Grid key={`subtask-${task.id}`} item>
-                                <Paper elevation={3}>
-                                    <Card>
-                                        <CardHeader 
-                                            title={
-                                                <TextField 
-                                                    type="text"
-                                                    value={task.value ? task.value : undefined}
-                                                    placeholder={task.label}
-                                                    label={task.label}
-                                                    onChange={(event)=> this.updateTaskLabel(task.id, event)}
-                                                />}
-                                            subheader={
-                                                <Fragment>
-                                                    <div>
-                                                        {`μ => sum: ${task.muSum}`}
-                                                    </div>
-                                                    <div>
-                                                        {`σ => sqrt (sum squared): ${task.sigmaSum}`}
-                                                    </div>
-                                                </Fragment>
-                                            }
-                                        />
-                                        <SubTasks
-                                            taskId = {task.id}
-                                            updateCalculations = {this.updateCalculations}
-                                        />
-                                        <CardActions>
-                                            <Button
-                                                variant="contained"
-                                                color="secondary"
-                                                onClick={ () => this.deleteTask(task.id) }
-                                            >
-                                                Delete Task
-                                            </Button>
-                                        </CardActions>
-                                    </Card>
-                                </Paper>
+                    <Typography  
+                        variant="subtitle2"
+                        align="center"
+                    >
+                        {"The programmer's estimation best friend"}
+                    </Typography>
+
+                    <Typography  
+                        variant="body1"
+                        align="center"
+                    >
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vel nulla sit amet magna rhoncus gravida nec quis elit. Quisque eu massa laoreet, varius dolor sed, dignissim tellus. Fusce ut euismod magna. Proin accumsan mi ac risus dictum, at commodo mauris lacinia. Nunc nec ex dignissim, pharetra nibh venenatis, lacinia magna. Nam eget aliquet eros. Donec luctus pharetra porta. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nulla vel turpis lorem. Proin laoreet, urna in aliquam tincidunt, lacus lectus varius ante, at varius nisl tellus et nisi. Etiam hendrerit, ligula non dictum egestas, ex nulla rhoncus turpis, eu laoreet odio purus et nisl. Nulla in nibh efficitur, luctus nunc eget, viverra purus.
+                    </Typography>
+                </Container>
+
+                <Container>
+                    <Grid item xs={12}>
+                        <Grid container justify="center" spacing={gridSpacing}>
+                            {tasks.map((task) => (
+                                <Grid key={`subtask-${task.id}`} item>
+                                    <Paper elevation={3}>
+                                        <Card>
+                                            <CardHeader 
+                                                title={
+                                                    <TextField 
+                                                        type="text"
+                                                        value={task.value ? task.value : undefined}
+                                                        placeholder={task.label}
+                                                        label={task.label}
+                                                        onChange={(event)=> this.updateTaskLabel(task.id, event)}
+                                                    />}
+                                                subheader={
+                                                    <Fragment>
+                                                        <div>
+                                                            {`μ => sum: ${task.muSum}`}
+                                                        </div>
+                                                        <div>
+                                                            {`σ => sqrt (sum squared): ${task.sigmaSum}`}
+                                                        </div>
+                                                    </Fragment>
+                                                }
+                                            />
+                                            <SubTasks
+                                                taskId = {task.id}
+                                                updateCalculations = {this.updateCalculations}
+                                            />
+                                            <CardActions>
+                                                <Button
+                                                    variant="contained"
+                                                    color="secondary"
+                                                    onClick={ () => this.deleteTask(task.id) }
+                                                >
+                                                    Delete Task
+                                                </Button>
+                                            </CardActions>
+                                        </Card>
+                                    </Paper>
+                                </Grid>
+                            ))}
+
+                            <Grid item>
+                                <Fab color="primary" aria-label="add" onClick={ () => this.addTask() }>
+                                    <AddIcon/>
+                                </Fab>
                             </Grid>
-                        ))}
 
-                        <Grid item>
-                            <Fab color="primary" aria-label="add" onClick={ () => this.addTask() }>
-                                <AddIcon/>
-                            </Fab>
                         </Grid>
-
                     </Grid>
-                </Grid>
-            </Container>
+                </Container>
+            </Box>
         );
     }
 }
