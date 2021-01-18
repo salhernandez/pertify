2173dd5d286453c74cbca9bac75bb2628807dce8
Salvador Hernandez <hernandezg.sal@gmail.com>
2021-01-17 21:35:06 -0800

    Fix spacing

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ if (process.env.NODE_ENV === 'production') {
     ReactGA.initialize(process.env.REACT_APP_TRACKING_ID);
 }
 
-
 class App extends Component {
     constructor(props){
         super(props);
